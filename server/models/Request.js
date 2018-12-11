450a0601f189d92e48a408f1e120be04e09eb71f
David Diez <ddiezr@gmail.com>
2018-12-11 17:27:53 +0100

    Moved some lines

--- server/models/Request.js
+++ server/models/Request.js
@@ -3,13 +3,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const requestSchema = new Schema({
-  musicianID: { type: Schema.Types.ObjectId, ref: "Musician" }
+  musicianID: { type: Schema.Types.ObjectId, ref: 'Musician' },
 }, {
-    timestamps: {
-      createdAt: 'created_at',
-      updatedAt: 'updated_at',
-    },
-  });
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
+  },
+});
 
 const Request = mongoose.model('Request', requestSchema);
 module.exports = Request;
