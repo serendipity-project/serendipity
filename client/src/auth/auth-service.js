340b6efd2b76ec229365c66b5fc2cd8b3219aac0
David Diez <ddiezr@gmail.com>
2018-12-11 18:53:22 +0100

    Added Switch buttons in form in signup

--- client/src/auth/auth-service.js
+++ client/src/auth/auth-service.js
@@ -1,36 +1,50 @@
 import axios from 'axios';
 
 export default class AuthService {
-  constructor() {
-    let service = axios.create({
-      baseURL: 'http://localhost:5000/api/auth',
-      withCredentials: true
-    });
-    this.service = service;
-  }
-  
-  signup = (username, password, campus, course) => {
-    return this.service.post('/signup', {username, password, campus, course})
-    .then(response => response.data)
-  }
+    constructor() {
+        let service = axios.create({
+            baseURL: 'http://localhost:5000/api/auth',
+            withCredentials: true
+        });
+        this.service = service;
+    }
 
-  login = (username, password) => {
-    return this.service.post('/login', {username, password})
-    .then(response => response.data)
-  }
+    signup = (username, password, email, host, musician) => {
+        return this.service.post('/signup', {
+                username,
+                password,
+                email,
+                host,
+                musician
+            })
+            .then(response => response.data)
+    }
 
-  logout = () => {
-    return this.service.get('/logout', {})
-    .then(response => response.data)
-  }
+    login = (username, password) => {
+        return this.service.post('/login', {
+                username,
+                password
+            })
+            .then(response => response.data)
+    }
 
-  loggedin = () => {
-    return this.service.get('/loggedin')
-    .then(response => response.data)
-  }
+    logout = () => {
+        return this.service.get('/logout', {})
+            .then(response => response.data)
+    }
 
-    edit = (username, campus, course) => {
-    return this.service.post('/edit', {username, campus, course})
-    .then(response => response.data)
+    loggedin = () => {
+        return this.service.get('/loggedin')
+            .then(response => response.data)
+    }
+
+    edit = (username, email, host,musician) => {
+        return this.service.post('/edit', {
+                username,
+                email,
+                host,
+                musician
+            })
+            .then(response => response.data)
     }
 }
