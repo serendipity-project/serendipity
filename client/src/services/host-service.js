cb46eaaad704edfc38395b4c219efd92d3bd14bb
PomPom248 <sofiroggero@gmail.com>
2018-12-12 14:14:16 +0100

    modifications

--- client/src/services/host-service.js
+++ client/src/services/host-service.js
@@ -5,7 +5,7 @@ import React, { Component } from 'react';
 class generalServices extends Component {
     constructor(props) {
         let service = axios.create({
-            baseURL: 'http://localhost:5000/api/',
+            baseURL: `${process.env.REACT_APP_API_URL}/host`,
             withCredentials: true
         });
         this.service = service;
