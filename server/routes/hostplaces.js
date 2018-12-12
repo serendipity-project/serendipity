5117dea507546488ecddb0fc59adb7c183d3db87
PomPom248 <sofiroggero@gmail.com>
2018-12-12 14:19:27 +0100

    modifications in routes

--- server/routes/hostplaces.js
+++ server/routes/hostplaces.js
@@ -4,17 +4,29 @@ const HostPlace = require('../models/HostPlace');
 
 hostPlaceRouter.post("/new-place", (req, res, next) => {
     const { address, date, price, capacity, initialTime, finishingTime, placeName } = req.body
-    
-    
+    if (address === '' || date === '' || price === '' || capacity === '' || initialTime === '' || finishingTime === '' || placeName === '') {
+        res.status(500).json({
+            message: 'Provide all the neccessary information',
+        });
+        return;
+    }
+    const newPlace = new HostPlace({ address, date, price, capacity, initialTime, finishingTime, placeName })
+    newPlace.save()
+        .then(() => {
+            res.status(200).json(newPlace)
+        })
+        .catch(() => {
+            res.status(500).json({
+                messsage: 'Saving mew location in database went wrong.'
+            })
+        })
 })
-router.post(‘/books/add’, (req, res, next) => {
-    const { title, author, description, rating } = req.body;
-    const newBook = new Book({ title, author, description, rating})
-    newBook.save()
-    .then((book) => {
-      res.redirect(‘/books’);
-    })
-    .catch((error) => {
-      console.log(error);
-    })
-   });
+
+hostPlaceRouter.post("/edit", (req, res, next) => {
+    const { address, date, price, capacity, initialTime, finishingTime, placeName } = req.body
+    for (key in update) {
+        if (update[key] == '') {
+            delete update[key]
+        }
+    }
+})
