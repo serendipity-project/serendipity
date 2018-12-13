6dd385a7cc1f8182b501f8c587651afe96c077f5
PomPom248 <sofiroggero@gmail.com>
2018-12-13 17:13:45 +0100

    change in hostplace.js

--- client/src/components/HostPlace/HostPlace.js
+++ client/src/components/HostPlace/HostPlace.js
@@ -30,7 +30,7 @@ class HostPlace extends Component {
         // const location ={};
         // location.longitude = -3.703790;
         // location.latitude = 40.416775;
-        console.log(location);
+        // console.log(location);
         const { address, date,initialTime ,finishingTime,price, capacity,location,placeName,availability,concertRequest} = this.state;
      
         this.service.new( address,date,initialTime ,finishingTime,price, capacity,location,placeName,availability,concertRequest)
