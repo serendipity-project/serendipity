7036959772c0891764569330a5e81544fa8ca0f9
David Diez <ddiezr@gmail.com>
2018-12-18 19:21:37 +0100

    heroku stable version

---
+++ server/public/precache-manifest.9e09e5785d496c795472e2aaccba086e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "faabf044c79a72ce97e9d348f53def48",
+    "url": "/static/media/logo.faabf044.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f1678d414cee991ac8ee",
+    "url": "/static/js/main.f1678d41.chunk.js"
+  },
+  {
+    "revision": "1d9dd2cb3ec3b72ed114",
+    "url": "/static/js/1.1d9dd2cb.chunk.js"
+  },
+  {
+    "revision": "f1678d414cee991ac8ee",
+    "url": "/static/css/main.49b33281.chunk.css"
+  },
+  {
+    "revision": "1d9dd2cb3ec3b72ed114",
+    "url": "/static/css/1.9e2fd156.chunk.css"
+  },
+  {
+    "revision": "57ade220c0cdbe2c82f78b45a1298e4f",
+    "url": "/index.html"
+  }
+];
--- server/public/service-worker.js
+++ server/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ace4c172bedbea93a1e214568026eef9.js"
+  "/precache-manifest.9e09e5785d496c795472e2aaccba086e.js"
 );
 
 workbox.clientsClaim();
